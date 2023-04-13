b4dc12ffd0b482d84f135a3354b9d06f9b47e427
DESKTOP-AUHA1PB\s.s.milanov <s.s.milanov@gmail.com>
2023-04-13 16:46:24 +0300

    Updating homework5

--- src/main/java/EncapsulationDemo/TestIt.java
+++ src/main/java/EncapsulationDemo/TestIt.java
@@ -3,7 +3,7 @@ package EncapsulationDemo;
 public class TestIt {
     public static void main(String[] args) {
 
-        Person lora = new Person("Lora", 8);
+        Person lora = new Person("Lora", 15);
         Person alex = new Person("Alex", 8);
 
         Product coffee = new Product("Coffee", 8);
